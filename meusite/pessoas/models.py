0dd2a89ca70892425878d0446ccb7b810b9b240e
renantescaro <renantescaro@yahoo.com.br>
2021-07-18 01:57:26 -0300

    model de pessoa

--- meusite/pessoas/models.py
+++ meusite/pessoas/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Pessoa(models.Model):
+    nome     = models.CharField(max_length=200)
+    email    = models.CharField(max_length=200)
+    telefone = models.CharField(max_length=20)
