3852829cbe94b4486b4b2e912923faa607b03689
renantescaro <renantescaro@yahoo.com.br>
2021-07-18 02:02:15 -0300

    adicionando dados do model de pessoas

--- meusite/pessoas/views.py
+++ meusite/pessoas/views.py
@@ -1,21 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Pessoa
 
 def listar(request):
     context = {
-        'pessoas': [
-            'Renan',
-            'Ana',
-            'Luana',
-        ],
+        'pessoas': Pessoa.objects.all()
     }
     return render(request, 'pessoas/listagem.html', context)
 
 
 def nova(request):
-    return HttpResponse('nova pessoa')
+    return render(request, 'pessoas/formulario.html', {})
 
 
-def editar(request):
-    return HttpResponse('Editar pessoa')
+def editar(request, pessoa_id):
+    context = {
+        'pessoa': Pessoa.objects.get(pk=pessoa_id)
+    }
+    return render(request, 'pessoas/formulario.html', context)
