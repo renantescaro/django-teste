3c177463aa6e5065e40fab116bee805c8de24871
renantescaro <renantescaro@yahoo.com.br>
2021-07-18 01:59:20 -0300

    parametro na rota

--- meusite/pessoas/urls.py
+++ meusite/pessoas/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.listar, name='listar'),
     path('nova', views.nova, name='nova'),
-    path('editar', views.editar, name='editar')
+    path('<int:pessoa_id>/editar', views.editar, name='editar')
 ]
