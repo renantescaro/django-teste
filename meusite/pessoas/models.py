d99998982e149b394cd8147b2d6e6c984010544e
renantescaro <renantescaro@yahoo.com.br>
2021-10-23 18:04:44 -0300

    adicionando metodo mágico para mostrar nome da pessoa no painel admin

--- meusite/pessoas/models.py
+++ meusite/pessoas/models.py
@@ -4,3 +4,6 @@ class Pessoa(models.Model):
     nome     = models.CharField(max_length=200)
     email    = models.CharField(max_length=200)
     telefone = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.nome
