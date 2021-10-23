19cba6102b78bb7bf254f3aa913ac6c3d7afcce0
renantescaro <renantescaro@yahoo.com.br>
2021-10-23 18:03:13 -0300

    metodos de manipulação, salvar, editar, nova etc

--- meusite/pessoas/views.py
+++ meusite/pessoas/views.py
@@ -1,20 +1,35 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.http.request import HttpRequest
+from django.shortcuts import redirect, render
 from .models import Pessoa
 
-def listar(request):
+
+def listar(request:HttpRequest):
     context = {
-        'pessoas': Pessoa.objects.all()
-    }
-    return render(request, 'pessoas/listagem.html', context)
+        'pessoas': Pessoa.objects.all(), }
+    return render(request, 'listagem.html', context)
 
 
-def nova(request):
-    return render(request, 'pessoas/formulario.html', {})
+def nova(request:HttpRequest):
+    return render(request, 'formulario.html', {})
 
 
-def editar(request, pessoa_id):
+def editar(request:HttpRequest, pessoa_id:int):
     context = {
-        'pessoa': Pessoa.objects.get(pk=pessoa_id)
-    }
-    return render(request, 'pessoas/formulario.html', context)
+        'pessoa': Pessoa.objects.get(pk=pessoa_id) }
+    return render(request, 'formulario.html', context)
+
+
+def salvar(request:HttpRequest):
+    if request.method == 'POST':
+        Pessoa(
+            nome     = request.POST.get('nome'),
+            email    = request.POST.get('email'),
+            telefone = request.POST.get('telefone'),
+        ).save()
+    return redirect('/pessoas')
+
+
+def apagar(request:HttpRequest, pessoa_id:int):
+    if pessoa_id is not None:
+        Pessoa.objects.filter(pk=pessoa_id).delete()
+    return redirect('/pessoas')
