e23903454ca5ff010af66d06ecc89d810e811a21
renantescaro <renantescaro@yahoo.com.br>
2021-10-23 18:03:45 -0300

    adicionando rota apagar e editar

--- meusite/pessoas/urls.py
+++ meusite/pessoas/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-
 from . import views
 
+
 urlpatterns = [
-    path('', views.listar, name='listar'),
-    path('nova', views.nova, name='nova'),
-    path('<int:pessoa_id>/editar', views.editar, name='editar')
+    path('',       views.listar, name='listar'),
+    path('nova',   views.nova,   name='nova'),
+    path('salvar', views.salvar, name='salvar'),
+    path('<int:pessoa_id>/apagar', views.apagar, name='apagar'),
+    path('<int:pessoa_id>/editar', views.editar, name='editar'),
 ]
